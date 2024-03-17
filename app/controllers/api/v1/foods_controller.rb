942795d39a6bfcee2a115615fff5d2f017f18cf0
soikawa <soikawa0108@gmail.com>
2024-03-17 23:59:59 +0900

    line_foods

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class FoodsController < ApplicationController
+      
       def index
         restaurant = Restaurant.find(params[:restaurant_id])
         foods = restaurant.foods
